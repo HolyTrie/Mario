a4734f5f6363f3b47787c799b777204a6c8727e0
20shaked20 <shakedlevi32@gmail.com>
2024-01-27 14:36:14 +0200

    lint

--- Assets/FlipByCollider.cs
+++ Assets/FlipByCollider.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class FlipByCollider : MonoBehaviour
 {
     MovingPlatform _mp;
-    
     void Start()
     {
         _mp = GameObject.Find("PigEnemy").GetComponent<MovingPlatform>();
@@ -14,7 +13,6 @@ public class FlipByCollider : MonoBehaviour
         Debug.Log(other.tag);
         if (other.tag == "Start")
             _mp.setDirection(true);
-        
         if (other.tag == "End")
             _mp.setDirection(false);
     }
